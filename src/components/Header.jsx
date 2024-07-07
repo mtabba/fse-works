cff75152d9d69fc79c39e324c7535172a310399c
Mohsin Tabba Ovais <ahme.mohsin@gmail.com>
2024-07-07 17:58:36 +0500

    header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
+import companyLogo from '../assets/companylogo.jpg'; 
 
 const Header = () => {
-
+const headerItem=['HOME', 'SERVICES', 'PROJECTS', 'ABOUT US', 'CONTACT US']
 
   return (
-    <header className="bg-gray-800 text-white">
-      <div className="container mx-auto flex justify-between items-center py-4">
-        <div className="text-xl font-bold">FSE Works</div>
+    <header className=" text-neutral-50 h-32">
+      <div className="container mx-auto flex justify-between items-center pt-6">
+      <div className="flex items-center">
+        <img src={companyLogo} alt="Company Logo" className="h-16 mr-10" /> {/* Adjust height and margin as needed */}
+        <span className="text-4xl font-bold text-left">MIND TECH
+        <div className="text-xl font-bold text-left">Packaging Technical Resources</div>
+        </span> 
+      </div>
         <nav>
-          <ul className="flex space-x-4">
-            <li><a href="#" className="hover:text-gray-300">Home</a></li>
-            <li><a href="#" className="hover:text-gray-300">About</a></li>
-            <li><a href="#" className="hover:text-gray-300">Services</a></li>
-            <li><a href="#" className="hover:text-gray-300">Portfolio</a></li>
-            <li><a href="#" className="hover:text-gray-300">Contact</a></li>
+          <ul className="flex space-x-12">
+            {headerItem.map((item, index)=> 
+            <li className="list-none hover:drop-shadow-xl" key={index}> {item}</li>
+            )}
           </ul>
         </nav>
       </div>
